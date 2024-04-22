b6f67303c1bab0db8d562ba32979c51848037b70
KenKoorn <35050@ma-web.nl>
2024-04-22 10:24:42 +0200

    photo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
 <body>
     <header>
         <div class="logo">
-            <img src="logo.png" alt="Logo">
+            <img src="DSC01246.jpg" alt="Logo">
         </div>
         <nav>
             <ul>
